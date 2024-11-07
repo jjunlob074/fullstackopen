40feb73f3ce47130cecf8622bd5473673c344758
jjunlob074 <jjunlob074@g.educaand.es>
2024-11-07 15:56:38 +0100

    final exercise unicafe 1.11

--- PART-1/unicafe/src/App.jsx
+++ PART-1/unicafe/src/App.jsx
@@ -1,39 +1,48 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Button = ({handleClick,text}) => (
+const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-)
+);
 
-const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>;
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
-const Statistics = ({good,neutral,bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
-  const average = total > 0 ? (good - bad) / total : 0;
-  const positiveFeedback = total > 0 ? (good/total)*100 : 0;
+  const average = total > 0 ? ((good - bad) / total).toFixed(1) : 0;
+  const positiveFeedback = total > 0 ? ((good / total) * 100).toFixed(1) : 0;
+
+
   return (
     <>
       {total === 0 ? (
         <p>No Feedback Given</p>
       ) : (
-        <>
-              <StatisticLine text="Good" value={good} />
-              <StatisticLine text="Neutral" value={neutral} />
-              <StatisticLine text="Bad" value={bad} />
-              <StatisticLine text="Total" value={total} />
-              <StatisticLine text="Average" value={average} />
-              <StatisticLine text="Positive Feedback" value={`${positiveFeedback}%`} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="Total" value={total} />
+            <StatisticLine text="Average" value={average} />
+            <StatisticLine text="+Feedback" value={`${positiveFeedback}%`} />
+          </tbody>
+        </table>
       )}
     </>
   );
-}
+};
+
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <>
@@ -44,7 +53,7 @@ const App = () => {
       <h2>Results</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
