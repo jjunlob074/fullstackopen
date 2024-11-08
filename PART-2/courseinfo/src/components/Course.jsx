4f47992b925ffbd3277bbbefe42b05fbf73e000a
jjunlob074 <jjunlob074@g.educaand.es>
2024-11-08 16:13:59 +0100

    actividades 2.2-2.3

--- PART-2/courseinfo/src/components/Course.jsx
+++ PART-2/courseinfo/src/components/Course.jsx
@@ -2,10 +2,12 @@ import Header from './Header';
 import Content from './Content';
 
 const Course = ({ course }) => {
+
     return (
         <div>
             <Header course={course.name} />
             <Content parts={course.parts} />
+            <p><strong>Total de ejercicios: {course.totalExercises}</strong></p>
         </div>
     );
 };
